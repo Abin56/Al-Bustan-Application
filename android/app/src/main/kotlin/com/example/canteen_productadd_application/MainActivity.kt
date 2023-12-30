d5ee8476e781ef5dbbbf6e198a56c0ce4c06af0a
Abin56 <abinjohn8089@gmail.com>
2023-12-30 10:51:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/canteen_productadd_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.canteen_productadd_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:canteen_productadd_application/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void>main()async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // home: AdminHomeScreen(),
+    );
+  }
+}
