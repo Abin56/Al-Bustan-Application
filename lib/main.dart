ad57925f8a0f586070f52c596e5857e531cd9088
sruthiraj <sruthijyothiraj@gmail.com>
2024-01-15 12:29:43 +0530

    first comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:canteen_productadd_application/firebase_options.dart';
 import 'package:canteen_productadd_application/view/login/loginScreen.dart';
-import 'package:canteen_productadd_application/view/navbar/navbar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
