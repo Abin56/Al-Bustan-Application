a0296e84aa43acd9556d3e7ea863c52d9d99e68e
Abin56 <abinjohn8089@gmail.com>
2024-01-19 15:34:27 +0530

    19 jan first commit --@edwin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:canteen_productadd_application/firebase_options.dart';
 import 'package:canteen_productadd_application/view/core/shared_pref/shared_pref_helper.dart';
-import 'package:canteen_productadd_application/view/home/deliveryadmin/navbar/navbar.dart';
 import 'package:canteen_productadd_application/view/pages/splash_screen/splash_screeen.dart';
 // import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
